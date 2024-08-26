65bd703081fa5be627cc1a050232c0f932d9aaa8
kethakishore143 <kishore1993keta@gmail.com>
2024-08-26 17:06:18 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Components/Loginpage/Login';
+import Onebox from './Components/Onebox/Onebox';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/reachinboxapp" element={<Login />} />
+      <Route path="/reachinboxapp/onebox" element={<Onebox />} />
+    </Routes>
   );
 }
 
