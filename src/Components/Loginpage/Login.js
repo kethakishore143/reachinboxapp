c941d8f093b873e3329236356a2c15443db74495
kethakishore143 <kishore1993keta@gmail.com>
2024-08-27 00:06:50 +0530

    css changed

--- src/Components/Loginpage/Login.js
+++ src/Components/Loginpage/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { FcGoogle } from "react-icons/fc";
 import ".//Login.css";
 import Header from '../Header/Header';
@@ -19,7 +20,9 @@ function Login() {
                         </div>
                     </div>
                     <div className='createaccountbtn-container'>
-                        <button className='create-ac-btn'>Create an Account</button>
+                        <Link to="/reachinboxapp/onebox">
+                            <button className='create-ac-btn'>Create an Account</button>
+                        </Link>
                         <p className='already_ac'>Already have an account? <span className='sigin'>Sign In</span></p>
                     </div>
                 </div>
